dc191b5ce25b9359a573163eef9dd3847949e99f
pedro <pedromonteirobittencourt@gmail.com>
2024-07-31 17:09:42 -0300

    feat: created skeleton actor_list template

---
+++ README.md
@@ -0,0 +1,28 @@
+# Film Inventory
+
+This is an inventory manager for movies, developed as part of the Odin Project course. It allows you to keep track of previously registered films and actors.
+
+## Features
+
+- Add new films to the inventory
+- Add new actors to the inventory
+- Search for films by title, genre, or release year
+- Search for actors by name or nationality
+- Update film information
+- Update actor information
+- Delete films from the inventory
+- Delete actors from the inventory
+
+## Technologies Used
+
+- Node.js
+- Express.js
+- JavaScript
+- MongoDB
+- EJS
+- HTML
+- CSS
+
+## Author
+
+Pedro Bittencourt
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+const testActors = [
+  {
+    name: 'Leonardo DiCaprio',
+    age: 46,
+    nationality: 'American',
+    picture: 'https://example.com/leonardo.jpg',
+    movies: ['Inception', 'Titanic', 'The Revenant'],
+  },
+  {
+    name: 'Natalie Portman',
+    age: 40,
+    nationality: 'Israeli-American',
+    picture: 'https://example.com/natalie.jpg',
+    movies: ['Black Swan', 'V for Vendetta', 'Thor'],
+  },
+  {
+    name: 'Denzel Washington',
+    age: 66,
+    nationality: 'American',
+    picture: 'https://example.com/denzel.jpg',
+    movies: ['Training Day', 'Fences', 'The Equalizer'],
+  },
+];
+
+router.get('/actors', (req, res, next) => {
+  res.render('./actor/actor_list', { actors: testActors });
+});
+
 module.exports = router;
