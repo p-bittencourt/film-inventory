fc76e8a66567fe251a321b607dfcbb7562974c6b
pedro <pedromonteirobittencourt@gmail.com>
2024-08-05 19:04:05 -0300

    refactor: Update actor update form and controller to handle validation and sanitization

--- controllers/actorController.js
+++ controllers/actorController.js
@@ -126,35 +126,96 @@ exports.actor_update_get = asyncHandler(async (req, res, next) => {
   res.render('./actor/actor_create', { actor: actor, movies: allMovies });
 });
 
-exports.actor_update_post = asyncHandler(async (req, res, next) => {
-  const {
-    first_name,
-    family_name,
-    date_of_birth,
-    date_of_death,
-    nationality,
-    picture,
-    movies,
-  } = req.body;
-  let movieIds = [];
-  if (Array.isArray(movies)) {
-    movieIds = movies;
-  } else if (movies) {
-    movieIds = [movies];
-  }
-  const actor = new Actor({
-    first_name,
-    family_name,
-    date_of_birth,
-    date_of_death,
-    nationality,
-    picture,
-    movies: movieIds,
-    _id: req.params.id,
-  });
-  const updatedActor = await Actor.findByIdAndUpdate(req.params.id, actor, {});
-  res.redirect(updatedActor.url);
-});
+exports.actor_update_post = [
+  // Validate and sanitize fields
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name is required'),
+  body('family_name')
+    .trim()
+    .optional({ checkFalsy: true })
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Invalid family name'),
+  body('date_of_birth')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate()
+    .withMessage('Invalid date of birth'),
+  body('date_of_death')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate()
+    .withMessage('Invalid date of death'),
+  body('nationality')
+    .trim()
+    .optional({ checkFalsy: true })
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Invalid value for nationality'),
+  body('picture')
+    .trim()
+    .optional({ checkFalsy: true })
+    .isURL()
+    .withMessage('Invalid url'),
+  body('movies.*').trim().escape(),
+
+  // Process request after validation and sanitization
+  asyncHandler(async (req, res, next) => {
+    // Extract validation errors from a request
+    const errors = validationResult(req);
+
+    const {
+      first_name,
+      family_name,
+      date_of_birth,
+      date_of_death,
+      nationality,
+      picture,
+      movies,
+    } = req.body;
+
+    let movieIds = [];
+    if (Array.isArray(movies)) {
+      movieIds = movies;
+    } else if (movies) {
+      movieIds = [movies];
+    }
+
+    const actor = new Actor({
+      first_name,
+      family_name,
+      date_of_birth,
+      date_of_death,
+      nationality,
+      picture,
+      movies: movieIds,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitzed value/errors messages.
+      const allMovies = await Movie.find().exec();
+      console.log(errors);
+      res.render('./actor/actor_create', {
+        actor: actor,
+        movies: allMovies,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data is valid. Save actor
+      const updatedActor = await Actor.findByIdAndUpdate(
+        req.params.id,
+        actor,
+        {}
+      );
+      res.redirect(updatedActor.url);
+    }
+  }),
+];
 
 // Actor delete
 exports.actor_delete = asyncHandler(async (req, res, next) => {
