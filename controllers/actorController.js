c1163715105e7635a60c6d343e1783095e856a80
pedro <pedromonteirobittencourt@gmail.com>
2024-08-05 19:01:08 -0300

    feat: treated and sanitized actor creation

--- controllers/actorController.js
+++ controllers/actorController.js
@@ -1,5 +1,6 @@
 const Actor = require('../models/actor');
 const Movie = require('../models/movie');
+const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 
 // Display all actors
@@ -24,36 +25,91 @@ exports.actor_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // Post actor form
-exports.actor_create_post = asyncHandler(async (req, res, next) => {
-  // Implement form handling
-  // Implement error handling
-  const {
-    first_name,
-    family_name,
-    date_of_birth,
-    date_of_death,
-    nationality,
-    picture,
-    movies,
-  } = req.body;
-  let movieIds = [];
-  if (Array.isArray(movies)) {
-    movieIds = movies;
-  } else if (movies) {
-    movieIds = [movies];
-  }
-  const actor = new Actor({
-    first_name,
-    family_name,
-    date_of_birth,
-    date_of_death,
-    nationality,
-    picture,
-    movies: movieIds,
-  });
-  await actor.save();
-  res.redirect(actor.url);
-});
+exports.actor_create_post = [
+  // Validate and sanitize fields
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name is required'),
+  body('family_name')
+    .trim()
+    .optional({ checkFalsy: true })
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Invalid family name'),
+  body('date_of_birth')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate()
+    .withMessage('Invalid date of birth'),
+  body('date_of_death')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate()
+    .withMessage('Invalid date of death'),
+  body('nationality')
+    .trim()
+    .optional({ checkFalsy: true })
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Invalid value for nationality'),
+  body('picture')
+    .trim()
+    .optional({ checkFalsy: true })
+    .isURL()
+    .withMessage('Invalid url'),
+  body('movies.*').trim().escape(),
+
+  // Process request after validation and sanitization
+  asyncHandler(async (req, res, next) => {
+    // Extract validation errors from a request
+    const errors = validationResult(req);
+
+    const {
+      first_name,
+      family_name,
+      date_of_birth,
+      date_of_death,
+      nationality,
+      picture,
+      movies,
+    } = req.body;
+
+    let movieIds = [];
+    if (Array.isArray(movies)) {
+      movieIds = movies;
+    } else if (movies) {
+      movieIds = [movies];
+    }
+
+    const actor = new Actor({
+      first_name,
+      family_name,
+      date_of_birth,
+      date_of_death,
+      nationality,
+      picture,
+      movies: movieIds,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitzed value/errors messages.
+      const allMovies = await Movie.find().exec();
+      console.log(errors);
+      res.render('./actor/actor_create', {
+        actor: actor,
+        movies: allMovies,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data is valid. Save actor
+      await actor.save();
+      res.redirect(actor.url);
+    }
+  }),
+];
 
 // Actor detail page
 exports.actor_detail = asyncHandler(async (req, res, next) => {
--- models/actor.js
+++ models/actor.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 
 const ActorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
-  family_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, maxLength: 100 },
   date_of_birth: { type: Date },
   date_of_death: { type: Date },
   nationality: { type: String },
