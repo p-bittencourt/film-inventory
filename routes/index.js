fb4312449dbfa9f91587d43f00fb7c7af863c443
pedro <pedromonteirobittencourt@gmail.com>
2024-08-08 15:49:08 -0300

    chore: start clearing unused files to prepare for version 1.0.0

--- routes/index.js
+++ routes/index.js
@@ -15,40 +15,7 @@ router.use('/directors', directorRouter);
 
 // Render index
 router.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-// SHOW ROUTES
-
-const testShows = [
-  {
-    title: 'Breaking Bad',
-    director: 'Vince Gilligan',
-    summary:
-      'A high school chemistry teacher turned methamphetamine producer partners with a former student to create and sell the purest methamphetamine.',
-    cast: ['Bryan Cranston', 'Aaron Paul'],
-    poster: 'https://example.com/breakingbad.jpg',
-  },
-  {
-    title: 'Game of Thrones',
-    director: 'David Benioff',
-    summary:
-      'Nine noble families fight for control over the lands of Westeros, while an ancient enemy returns after being dormant for millennia.',
-    cast: ['Emilia Clarke', 'Kit Harington'],
-    poster: 'https://example.com/gameofthrones.jpg',
-  },
-  {
-    title: 'Stranger Things',
-    director: 'The Duffer Brothers',
-    summary:
-      'In a small town where everyone knows everyone, a group of kids uncover a series of supernatural mysteries and government conspiracies.',
-    cast: ['Millie Bobby Brown', 'Finn Wolfhard'],
-    poster: 'https://example.com/strangerthings.jpg',
-  },
-];
-
-router.get('/shows', (req, res, next) => {
-  res.render('./show/show_list', { shows: testShows });
+  res.redirect('/movies');
 });
 
 module.exports = router;
