225cec0fb73bedd46729157a34215a1508af0ada
pedro <pedromonteirobittencourt@gmail.com>
2024-10-21 17:37:27 -0300

    Refactor: Add password authentication to actor and director routes

--- routes/actorRouter.js
+++ routes/actorRouter.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
 const actor_controller = require('../controllers/actorController');
+const checkPassword = require('../middleware/checkPassword');
 
 router.get('/', actor_controller.actor_list);
 
 router.get('/create', actor_controller.actor_create_get);
 
-router.post('/create', actor_controller.actor_create_post);
+router.post('/create', checkPassword, actor_controller.actor_create_post);
 
 router.get('/edit/:id', actor_controller.actor_update_get);
 
-router.post('/edit/:id', actor_controller.actor_update_post);
+router.post('/edit/:id', checkPassword, actor_controller.actor_update_post);
 
-router.delete('/:id', actor_controller.actor_delete);
+router.delete('/:id', checkPassword, actor_controller.actor_delete);
 
 router.get('/:id', actor_controller.actor_detail);
 
--- routes/directorRouter.js
+++ routes/directorRouter.js
@@ -1,17 +1,22 @@
 const router = require('express').Router();
 const director_controller = require('../controllers/directorController');
+const checkPassword = require('../middleware/checkPassword');
 
 router.get('/', director_controller.director_list);
 
 router.get('/create', director_controller.director_create_get);
 
-router.post('/create', director_controller.director_create_post);
+router.post('/create', checkPassword, director_controller.director_create_post);
 
 router.get('/edit/:id', director_controller.director_update_get);
 
-router.post('/edit/:id', director_controller.director_update_post);
+router.post(
+  '/edit/:id',
+  checkPassword,
+  director_controller.director_update_post
+);
 
-router.delete('/:id', director_controller.director_delete);
+router.delete('/:id', checkPassword, director_controller.director_delete);
 
 router.get('/:id', director_controller.director_detail);
 
